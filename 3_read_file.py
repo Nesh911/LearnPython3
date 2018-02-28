7e18e3fe1a22208223c52eeb627c549ee3df8790
Viktor Zagvoskin <v.zagvoskin@rambler.ru>
2018-02-28 20:56:41 +0300

    28.02 Добавил решение третьего задания.

---
+++ 3_read_file.py
@@ -0,0 +1,19 @@
+# Задание - Прочитайте его и подсчитайте количество слов в тексте файла 'referat.txt'
+
+def read_file():
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        print(str(sum(1 for i in f)) + ' строк в файле, включая пустые')
+
+
+def read_file2():
+    final_list = []
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            if not line.isspace():
+                word = line.split(' ')
+                for i in word:
+                    final_list.append(i.strip())
+    print(str(len(final_list)) + ' слова в тексте')
+
+read_file()
+read_file2()
